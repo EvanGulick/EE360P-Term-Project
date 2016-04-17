603c2f1642ffafa4816a2a0be5a9134ed9a5d2c2
Evan Gulick <evanglck12@utexas.edu>
2016-04-17 14:24:18 -0500

    ReaderWriter Implicit and Explicit

--- ReaderWriter/src/readerWriterExplicit.java
+++ ReaderWriter/src/readerWriterExplicit.java
@@ -1,4 +1,61 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class readerWriterExplicit {
-
+	int readerCount;
+	int writerCount;
+	int writerSleeping;
+	private final Lock lock = new ReentrantLock();
+	
+	private final Condition readerWait = lock.newCondition();
+	private final Condition writerWait = lock.newCondition();
+	
+	public readerWriterExplicit(){
+		readerCount = 0;
+		writerCount = 0;
+		writerSleeping = 0;
+	}
+	
+	public void startRead(){
+		if(writerCount != 0 || writerSleeping != 0){
+			try {
+				readerWait.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		readerCount += 1;
+		readerWait.signalAll();
+	}
+	
+	public void endRead(){
+		readerCount -= 1;
+		if(readerCount == 0){
+			writerWait.signal();
+		}
+	}
+	
+	public void startWrite(){
+		if(readerCount != 0 || writerCount != 0){
+			try {
+				writerSleeping += 1;
+				writerWait.await();
+				writerSleeping -= 1;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		writerCount = 1;
+	}
+	
+	public void endWrite(){
+		writerCount = 0;
+		if(writerSleeping != 0){
+			writerWait.signal();
+		}
+		else{
+			readerWait.signalAll();
+		}
+	}
 }
--- ReaderWriter/src/readerWriterImplicit.java
+++ ReaderWriter/src/readerWriterImplicit.java
@@ -1,4 +1,28 @@
 
 public class readerWriterImplicit {
-
+	int readerCount;
+	int writerCount;
+	
+	public readerWriterImplicit(){
+		readerCount = 0;
+		writerCount = 0;
+	}
+	
+	public void startRead(){
+		while(writerCount == 0){}
+		readerCount += 1;
+	}
+	
+	public void endRead(){
+		readerCount -= 1;
+	}
+	
+	public void startWrite(){
+		while(readerCount == 0 && writerCount == 0){}
+		writerCount = 1;
+	}
+	
+	public void endWrite(){
+		writerCount = 0;
+	}
 }
