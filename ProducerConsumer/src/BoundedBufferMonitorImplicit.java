8ce517c78913345425354ad9643254b1b1dff1b8
safeen713 <safeen713@gmail.com>
2016-04-23 15:03:46 -0500

    add implicit files for producer consumer

---
+++ ProducerConsumer/src/BoundedBufferMonitorImplicit.java
@@ -0,0 +1,29 @@
+public class BoundedBufferMonitorImplicit {
+
+    final int size = 10;
+    Object[] buffer = new Object[size];
+    int inBuf = 0, outBuf = 0, count = 0;
+    public synchronized void deposit(Object value) {
+        while (count == size) // buffer full
+            myUtil.myWait(this);
+    	//while(count == size){}
+        buffer[inBuf] = value;
+        inBuf = (inBuf + 1) % size;
+        count++;
+        if (count == 1) // items available for fetch
+            notify();
+    }
+    public synchronized Object fetch() {
+        Object value;
+        while (count == 0) // buffer empty
+            myUtil.myWait(this);
+        //while(count == 0){}
+        value = buffer[outBuf];
+        outBuf = (outBuf + 1) % size;
+        count--;
+        if (count == size - 1) // empty slots available
+            notify();
+        return value;
+    }
+
+}
